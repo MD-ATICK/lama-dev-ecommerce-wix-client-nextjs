7820ac5f2dde66783bcfab74a42e69dce4fef1dd
MD Atick <115481550+MD-ATICK@users.noreply.github.com>
2024-10-04 16:00:07 +0600

    web name change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,41 +8,41 @@ import "./globals.css";
 const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'ShopNifty by Atick - Best Online Store in Bangladesh for Unique Products',
-  description: 'ShopNifty by Atick offers a curated collection of stylish and innovative products across fashion, electronics, home goods, and more. Discover the best deals tailored for shoppers in Bangladesh.',
-  keywords: 'ShopNifty by Atick, unique products Bangladesh, online shopping BD, fashion deals BD, electronics BD, home decor BD, exclusive deals Bangladesh, best online store BD',
+  title: 'ShopNifty - Best Online Store in Bangladesh for Unique Products',
+  description: 'ShopNifty offers a curated collection of stylish and innovative products across fashion, electronics, home goods, and more. Discover the best deals tailored for shoppers in Bangladesh.',
+  keywords: 'ShopNifty, unique products Bangladesh, online shopping BD, fashion deals BD, electronics BD, home decor BD, exclusive deals Bangladesh, best online store BD',
   icons: {
     icon: '/favicon.ico',
   },
   openGraph: {
-    title: 'ShopNifty by Atick - Best Online Store in Bangladesh',
-    description: 'Explore ShopNifty by Atick for a handpicked collection of trendy and high-quality products. Get exclusive deals on fashion, electronics, and home essentials, perfect for shoppers in Bangladesh.',
-    url: 'https://shopNiftybyatick.vercel.app',
+    title: 'ShopNifty - Best Online Store in Bangladesh',
+    description: 'Explore ShopNifty for a handpicked collection of trendy and high-quality products. Get exclusive deals on fashion, electronics, and home essentials, perfect for shoppers in Bangladesh.',
+    url: 'https://shopnifty.vercel.app',
     type: 'website',
     locale: 'en_BD',
     images: [
       {
-        url: 'https://shopNiftybyatick.vercel.app/og-image.jpg',
+        url: 'https://shopnifty.vercel.app/og-image.jpg',
         width: 1200,
         height: 630,
-        alt: 'ShopNifty by Atick - Trendy and Unique Products for Bangladesh',
+        alt: 'ShopNifty - Trendy and Unique Products for Bangladesh',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'ShopNifty by Atick - Best Online Store in Bangladesh',
-    description: 'Find exclusive deals on a wide range of trendy and unique products for Bangladeshi shoppers. From fashion to home essentials, ShopNifty by Atick has it all.',
-    images: ['https://shopNiftybyatick.vercel.app/twitter-image.jpg'],
+    title: 'ShopNifty - Best Online Store in Bangladesh',
+    description: 'Find exclusive deals on a wide range of trendy and unique products for Bangladeshi shoppers. From fashion to home essentials, ShopNifty has it all.',
+    images: ['https://shopnifty.vercel.app/twitter-image.jpg'],
   },
   robots: {
     index: true,
     follow: true,
   },
   alternates: {
-    canonical: 'https://shopNiftybyatick.vercel.app',
+    canonical: 'https://shopnifty.vercel.app',
     languages: {
-      'en-BD': 'https://shopNiftybyatick.vercel.app/bd',
+      'en-BD': 'https://shopnifty.vercel.app/bd',
     },
   }
 };
@@ -55,6 +55,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="6GmT2Do1lN7VtwEFDc82A70PsvPJHHw07NtB75FFIew" />
+      </head>
       <body className={`${inter.className} scrollBar`}>
         <WinClientContextProvider>
           <Navbar />
